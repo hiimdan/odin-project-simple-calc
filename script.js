793768110e6052ab81be12ca04b5f340f761b72c
Dan <48810799+hiimdan@users.noreply.github.com>
2022-12-21 20:09:21 -0600

    Create button layout and map number input to display

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const numberButtons = document.querySelectorAll('.number');
+numberButtons.forEach(btn => {
+    btn.addEventListener('click', inputNumber)
+})
+
+const clearButton = document.querySelector('#clear');
+clearButton.addEventListener('click', () => calcDisplay.textContent = '');
+
+const calcDisplay = document.querySelector('#display');
+
 function operate(left, right, operand) {
     if (operand == '+') {
         return left + right;
@@ -11,4 +21,8 @@ function operate(left, right, operand) {
     if (operand == '/') {
         return left / right;
     }
+}
+
+function inputNumber(e) {
+    calcDisplay.textContent += e.target.textContent;
 }
