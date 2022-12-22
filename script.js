b450cd652cbbab796bf91bf252aeab01f721f8bc
Dan <48810799+hiimdan@users.noreply.github.com>
2022-12-21 23:32:04 -0600

    Add ability to perform basic arithmetic, handle equal operation, handle decimal button

--- script.js
+++ script.js
@@ -4,9 +4,26 @@ numberButtons.forEach(btn => {
 })
 
 const clearButton = document.querySelector('#clear');
-clearButton.addEventListener('click', () => calcDisplay.textContent = '');
+clearButton.addEventListener('click', clearScreen);
+
+const operatorButtons = document.querySelectorAll('.operator');
+operatorButtons.forEach(btn => {
+    btn.addEventListener('click', handleOperator);
+})
+
+const equalsButton = document.querySelector('#equals');
+equalsButton.addEventListener('click', handleEquals);
+
+const decimalButton = document.querySelector('#decimal');
+decimalButton.addEventListener('click', handleDecimal);
 
 const calcDisplay = document.querySelector('#display');
+
+let prevValue = '';
+let currentValue = '';
+let currentOperator = null;
+
+let equalsPressed = false;
 
 function operate(left, right, operand) {
     if (operand == '+') {
@@ -15,7 +32,7 @@ function operate(left, right, operand) {
     if (operand == '-') {
         return left - right;
     }
-    if (operand == '*') {
+    if (operand == 'x') {
         return left * right;
     }
     if (operand == '/') {
@@ -24,5 +41,69 @@ function operate(left, right, operand) {
 }
 
 function inputNumber(e) {
-    calcDisplay.textContent += e.target.textContent;
+    if (currentOperator === null) {
+        if (equalsPressed) {
+            prevValue = e.target.textContent;
+            equalsPressed = false;
+        } else {
+            prevValue += e.target.textContent;
+        }
+        calcDisplay.textContent = prevValue;
+    } else {
+        currentValue += e.target.textContent;
+        calcDisplay.textContent = currentValue;
+    }
+}
+
+function clearScreen() {
+    calcDisplay.textContent = '';
+    prevValue = '';
+    currentValue = '';
+    currentOperator = null;
+    equalsPressed = false;
+}
+
+function handleOperator(e) {
+    if (calcDisplay.textContent) {
+        if (currentOperator === null || !currentValue) {
+            currentOperator = e.target.textContent;
+        } else {
+            let result = operate(parseFloat(prevValue), parseFloat(currentValue), currentOperator);
+            prevValue = result;
+            calcDisplay.textContent = result;
+            currentOperator = e.target.textContent;
+            currentValue = '';
+        }
+    }
+}
+
+function handleEquals() {
+    if (prevValue && currentValue) {
+        let result = operate(parseFloat(prevValue), parseFloat(currentValue), currentOperator);
+        prevValue = result;
+        calcDisplay.textContent = result;
+        currentValue = '';
+        currentOperator = null;
+        equalsPressed = true;
+    }
+}
+
+function handleDecimal() {
+    if (currentOperator === null) {
+        if (!prevValue) {
+            calcDisplay.textContent = '0.';
+            prevValue = '0.';
+        } else if (!/\./.test(prevValue)) {
+            calcDisplay.textContent += '.';
+            prevValue += '.';
+        }
+    } else {
+        if (!currentValue) {
+            calcDisplay.textContent = '0.';
+            currentValue = '0.';
+        } else if (!/\./.test(currentValue)) {
+            calcDisplay.textContent += '.';
+            currentValue += '.';
+        }
+    }
 }
